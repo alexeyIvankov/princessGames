65edb940f530cc08a9ee41af2048203a570a814f
Ghost257 <iknow.mclub@gmail.com>
2019-07-20 10:37:03 +0300

    Correct errors with ShowRoom Scene

--- Map3D/Assets/Scripts/ShowRoom.cs
+++ Map3D/Assets/Scripts/ShowRoom.cs
@@ -19,28 +19,34 @@ namespace Scenes
         public CharacterList characterList;
         public int currentCharacter;
         public TextMeshProUGUI characterName;
+
+        private Boolean visible;
         // Start is called before the first frame update
+
+       
         void Start()
         {
-            //Getting a list with characters
-            characterList = new CharacterList();
-            for (int i = 0; i < characterList.GetLength(); i++)
-            {
-                //Create list that contains prefabs of characters
-                prefabs.Add(Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(i).prefabName));
-            }
-            currentCharacter = FindObjectOfType<AppPreferences>().GetCurrentCharacter();
-           // Debug.Log(currentCharacter);
-           // Debug.Log(FindObjectOfType<Selector>().ToString());
-           // Debug.Log(currentCharacter.ToString());
-           //Writing the name of current character
-            characterName.text = characterList.GetCharacter(currentCharacter).characterName;
-           // prefab = Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(currentCharacter).prefabName);
-           //Drawing a character
-            character = Instantiate(prefabs[currentCharacter], characterTransform.position, characterTransform.rotation);
-            character.transform.RotateAround(characterTransform.position, Vector3.up, 180f);
-            isUpToDate = true;
+                //Getting a list with characters
+                characterList = new CharacterList();
+                for (int i = 0; i < characterList.GetLength(); i++)
+                {
+                    //Create list that contains prefabs of characters
+                    prefabs.Add(Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(i).prefabName));
+                }
 
+                currentCharacter = FindObjectOfType<AppPreferences>().GetCurrentCharacter();
+                // Debug.Log(currentCharacter);
+                // Debug.Log(FindObjectOfType<Selector>().ToString());
+                // Debug.Log(currentCharacter.ToString());
+                //Writing the name of current character
+                characterName.text = characterList.GetCharacter(currentCharacter).characterName;
+                // prefab = Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(currentCharacter).prefabName);
+                //Drawing a character
+                character = Instantiate(prefabs[currentCharacter], characterTransform.position,
+                    characterTransform.rotation);
+                character.transform.RotateAround(characterTransform.position, Vector3.up, 180f);
+                isUpToDate = true;
+            
 
 
         }
@@ -48,52 +54,57 @@ namespace Scenes
         // Update is called once per frame
         void Update()
         {
-            //Touch screen swipe control
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-                switch (touch.phase)
+          
+
+                //Touch screen swipe control
+                if (Input.touchCount > 0)
                 {
-                    case TouchPhase.Began:
-                        initialPos = touch.position;
-                        break;
-                    case TouchPhase.Moved:
-                        delatPos = initialPos - touch.position;
-                        Debug.Log("Delta Position " + delatPos.x);
-                        if (delatPos.x < -100f)
-                        {
-                            Debug.Log("Right");
-                            //Rotate Right
-                            character.transform.RotateAround(characterTransform.position, Vector3.up, -10f);
-                        }
-                        else if (delatPos.x > 100f)
-                        {
-                            Debug.Log("Left");
-                            //Rotate Left
-                            character.transform.RotateAround(characterTransform.position, Vector3.up, 10f);
-                        }
-                        break;
+                    Touch touch = Input.GetTouch(0);
+                    switch (touch.phase)
+                    {
+                        case TouchPhase.Began:
+                            initialPos = touch.position;
+                            break;
+                        case TouchPhase.Moved:
+                            delatPos = initialPos - touch.position;
+                            Debug.Log("Delta Position " + delatPos.x);
+                            if (delatPos.x < -100f)
+                            {
+                                Debug.Log("Right");
+                                //Rotate Right
+                                character.transform.RotateAround(characterTransform.position, Vector3.up, -10f);
+                            }
+                            else if (delatPos.x > 100f)
+                            {
+                                Debug.Log("Left");
+                                //Rotate Left
+                                character.transform.RotateAround(characterTransform.position, Vector3.up, 10f);
+                            }
+
+                            break;
+                    }
+
+
+
+
                 }
 
-               
-                    
-            }
-            
-            if (!isUpToDate)
-            {
-                //Update character and character name when new character is chosen
-                character.SetActive(false);
-                //prefabs = Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(currentCharacter).prefabName);
-                characterName.text = characterList.GetCharacter(currentCharacter).characterName;
-                character = Instantiate(prefabs[currentCharacter], characterTransform.position, characterTransform.rotation);
-                character.transform.RotateAround(characterTransform.position, Vector3.up, 180f);
-                FindObjectOfType<AppPreferences>().SetCurrentCharacter(currentCharacter);
-                isUpToDate = true;
-            }
+                if (!isUpToDate)
+                {
+                    //Update character and character name when new character is chosen
+                    character.SetActive(false);
+                    //prefabs = Resources.Load<GameObject>("Prefabs/" + characterList.GetCharacter(currentCharacter).prefabName);
+                    characterName.text = characterList.GetCharacter(currentCharacter).characterName;
+                    character = Instantiate(prefabs[currentCharacter], characterTransform.position,
+                        characterTransform.rotation);
+                    character.transform.RotateAround(characterTransform.position, Vector3.up, 180f);
+                    FindObjectOfType<AppPreferences>().SetCurrentCharacter(currentCharacter);
+                    isUpToDate = true;
+                }
+
+
         }
-        
-       
-        }
-     
+    }
+
 }
 
